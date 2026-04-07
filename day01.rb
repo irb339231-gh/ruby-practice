8d7710c52ed4f569085f755c53d260ff5e3a3e55
Koki Matsui <irb339231@gmail.com>
2026-04-07 19:01:44 +0900

    day01:変数、配列、ハッシュ

---
+++ day01.rb
@@ -0,0 +1,15 @@
+#変数
+name = "matsuikoki"
+score = 100
+puts name
+puts score
+
+#配列
+ranking = ["Alice", "Bob", "Carol"]
+puts ranking[0]
+puts ranking.length
+
+#ハッシュ
+player = { "name" => "matsuikoki", "score" => 100 }
+puts player["name"]
+puts player["score"]
