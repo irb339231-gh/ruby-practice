caf36707b3883b0ab43efc8f2c8b6735c3b924aa
Koki Matsui <irb339231@gmail.com>
2026-04-26 11:19:29 +0900

    乱数

---
+++ 12_7.rb
@@ -0,0 +1,15 @@
+p Random.rand
+p Random.rand(10)
+p Random.rand(1..10)
+
+r1 = Random.new(1)
+p [r1.rand, r1.rand]
+
+r2 = Random.new(1)
+p [r2.rand, r2.rand]
+
+r3 = Random.new
+p [r3.rand, r3.rand]
+
+srand(42)
+p rand(10)
