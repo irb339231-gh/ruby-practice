1eae1a314a03f9e23df6c95fd87a644bc48d2466
Koki Matsui <irb339231@gmail.com>
2026-05-10 13:41:35 +0900

    文字列

---
+++ 14_1.rb
@@ -0,0 +1,3 @@
+str = "Ruby is an object oriented programming language."
+column = str.split(" ")
+p column
---
+++ 14_2.rb
@@ -0,0 +1,4 @@
+str = "Ruby is an object oriented programming language."
+column = str.split(" ")
+sorted_column = column.sort
+p sorted_column
